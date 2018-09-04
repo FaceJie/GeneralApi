2e7a2a8f9fb545e240feb53683f985b898c5e0b3
FaceJie <xiang199557@qq.com>
2018-09-04 11:36:05 +0800

    框架的搭建

---
+++ APICarrier/Areas/CRUD_Common/CRUD_CommonAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace APICarrier.Areas.CRUD_Common
+{
+    public class CRUD_CommonAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CRUD_Common";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CRUD_Common_default",
+                "CRUD_Common/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "APICarrier.Areas.CRUD_Common.Controllers" }
+            );
+        }
+    }
+}
---
+++ APICarrier/Areas/CRUD_Common/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APICarrier.Areas.CRUD_Common.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: CRUD_Common/Base
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
